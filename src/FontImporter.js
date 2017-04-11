a355d8eda17ead1f4915f504d0fc47fa2f4bc9da
Joshua Thornton <joshua.thornton@heg.com>
2017-04-11 14:17:10 -0700

    Initial codebase codebase.
    feat: given an array of urls it will import the fonts.

---
+++ src/FontImporter.js
@@ -0,0 +1,53 @@
+import React, { PropTypes } from 'react';
+import {
+  setPropTypes,
+  setDisplayName,
+  compose,
+  mapProps,
+} from 'recompose';
+import { map, compose as rCompose, join } from 'ramda';
+
+const FontImporter = ({
+  children,
+  css,
+}) => {
+  /* eslint-disable react/no-danger */
+  return (
+    <div>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: css,
+        }}
+      />
+      {children}
+    </div>
+  );
+  /* eslint-enable react/no-danger */
+};
+
+export const PureComponent = FontImporter;
+
+export const mapUrlsToImports = map((url) => {
+  return `@import url('${url}');`;
+});
+
+export const mapImportsToCss = rCompose(
+  join('\n'),
+  mapUrlsToImports,
+);
+
+export const enhance = compose(
+  setDisplayName('FontImporter'),
+  setPropTypes({
+    children: PropTypes.node,
+    urls: PropTypes.array.isRequired,
+  }),
+  mapProps((ownerProps) => {
+    return {
+      children: ownerProps.children,
+      css: mapImportsToCss(ownerProps.urls),
+    };
+  }),
+);
+
+export default enhance(FontImporter);
